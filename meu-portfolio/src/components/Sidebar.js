611fa100b695e9f69e05f68b9488820399762dba
CaiozinS2 <caioandersongoes@gmail.com>
2024-08-25 01:32:19 -0300

    Finalizando a Sidebar

--- meu-portfolio/src/components/Sidebar.js
+++ meu-portfolio/src/components/Sidebar.js
@@ -2,26 +2,59 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import profilePic from '../assets/perfil-1.jpeg';
 import ThemeToggle from './ThemeToggle';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import AutoStoriesOutlinedIcon from '@mui/icons-material/AutoStoriesOutlined';
+import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
+import PermContactCalendarOutlinedIcon from '@mui/icons-material/PermContactCalendarOutlined';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className='profile-section'>
         <img src={profilePic} alt="Perfil" className="profile-pic" />
-        <p>Caio Anderson</p>
-        <ThemeToggle />
+        <h2 className='titulo'>Caio Anderson</h2>
+        <p className='subtitulo'>Desenvolvedor Front-End</p>
       </div>
       <nav>
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">Sobre Mim</a></li>
-          <li><a href="#skills">Habilidades</a></li>
-          <li><a href="#projects">Projetos</a></li>
-          <li><a href="#contact">Contato</a></li>
+          <li>
+            <a href="#home">
+              <HomeOutlinedIcon />
+              <p>Home</p>
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              <PersonOutlinedIcon />
+              <p>Sobre Mim</p>
+            </a>
+          </li>
+          <li>
+            <a href="#skills">
+              <AutoStoriesOutlinedIcon />
+              <p>Habilidades</p>
+            </a>
+          </li>
+          <li>
+            <a href="#projects">
+              <AssignmentTurnedInOutlinedIcon />
+              <p>Projetos</p>
+            </a>
+          </li>
+          <li>
+            <a href="#contact">
+              <PermContactCalendarOutlinedIcon />
+              <p>Contato</p>
+            </a>
+          </li>
         </ul>
+        <div className='theme-toggle'>
+          <ThemeToggle />
+        </div>
       </nav>
       <div className='footer-sidebar'>
-        <p>&copy; Copyright</p>
+        <p>&copy; Copyright Portfólio</p>
         <p>Desenvolvido por Caio Anderson.</p>
       </div>
     </div>
