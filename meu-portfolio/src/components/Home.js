776f8c8f161cb6d0927943907275de72156ebe5a
CaiozinS2 <caioandersongoes@gmail.com>
2024-08-27 18:10:44 -0300

    Em Andamento

--- meu-portfolio/src/components/Home.js
+++ meu-portfolio/src/components/Home.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import { Fade, Box, Typography } from '@mui/material';
+import { Fade, Box, Typography, Button, Slide } from '@mui/material';
 
 const Home = () => {
   return (
     <Fade in={true} timeout={2500}>
-      <Box className='home' sx={{ textAlign: 'center', mt: 4 }}>
-        <Typography variant="h1">Bem-vindo ao Meu Portfólio</Typography>
-        <Typography variant="body1">Explore meus projetos e habilidades.</Typography>
+      <Box 
+        className='home' 
+        sx={{ 
+          textAlign: 'center', 
+          mt: 0, 
+          display: 'flex', 
+          flexDirection: 'column', 
+          alignItems: 'center', 
+          justifyContent: 'center', 
+          height: '100vh',
+          backgroundImage: 'linear-gradient(135deg, #b4bfff 0%, #21d4fd 100%)'
+        }}
+      >
+        <Fade in={true} timeout={1000}>
+          <Typography variant="h2" sx={{ color: '#000', fontWeight: 'bold' }}>
+            Bem-vindo ao meu Portfólio!
+          </Typography>
+        </Fade>
+        <Fade in={true} timeout={5000}>
+          <Typography variant="body1" sx={{ color: '#0f0414', mt: 2 }}>
+            Aqui será como um laboratório, feito para diversos experimentos.
+          </Typography>
+        </Fade>
+        <Fade in={true} timeout={8000}>
+          <Button variant="contained" sx={{ mt: 4, backgroundColor: '#000', color: '#fff' }}>
+            Veja meus projetos
+          </Button>
+        </Fade>
       </Box>
     </Fade>
   );
